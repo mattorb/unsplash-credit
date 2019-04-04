88b3b38a4ff0a279be6794322999293356eabc5a
mattorb <matt@piikeastreet.com>
2019-04-04 00:15:27 -0500

    Support parsing name and username from image url
    Build the link to unsplash witht the username
    Build the caption with the name

--- unsplash-credit.js
+++ unsplash-credit.js
@@ -2,12 +2,13 @@ function add_unsplash_credit() {
     var el = document.querySelector( "#site-main > div > article > figure" );
     
     if (el && el.style.backgroundImage) {
-        var re = /(.*)\/([\w-]+)-([0-9]+)-unsplash\.jpg\"\)/g;
-        var photog_credit = el.style.backgroundImage.replace(re, '$2');
+        var re = /(.*)\/([\w-]+)-@([\w]+)-([0-9]+)-unsplash\.jpg\"\)/g;
+        var photog_name = el.style.backgroundImage.replace(re, '$2');
+        var photog_user = el.style.backgroundImage.replace(re, '$3');
     
         var credit_frag = `
-            <div style="text-align: right;">
-                photo credit: <a href="https://unsplash.com/user/${photog_credit}" target="_blank" rel="noopener noreferrer" title="Photo courtesy of ${photog_credit} on Unsplash"><span style="display:inline-block;padding:2px 3px;">${photog_credit}</span></a>
+            <div style="text-align: right; font-size: x-small;">
+                photo credit: <a href="https://unsplash.com/@${photog_user}" target="_blank" rel="noopener noreferrer" title="Photo courtesy of ${photog_name} on Unsplash"><span style="display:inline-block;padding:2px 3px;">${photog_name}</span></a>
             </div>
         `;
         
