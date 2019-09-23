9a0f2efb663f9786c8743d6fa97b93baac8b0b37
mattorb <matt@piikeastreet.com>
2019-09-23 08:37:05 -0500

    Update for Ghost/Casper 2.31

--- unsplash-credit.js
+++ unsplash-credit.js
@@ -1,13 +1,13 @@
 function add_unsplash_credit() {
-    var el = document.querySelector( "#site-main > div > article > figure" );
+    var el = document.querySelector( "#site-main > div > article > figure > img" );
     
-    if (el && el.style.backgroundImage) {
-        var re = /(.*)\/([\w-]+)-@([\w]+)-([a-zA-Z0-9]+)-unsplash\.jpg\"\)/g;
-        var photog_name = el.style.backgroundImage.replace(re, '$2');
-        var photog_user = el.style.backgroundImage.replace(re, '$3');
+    if (el && el.src) {
+        var re = /(.*)\/([\w-]+)-@([\w]+)-([a-zA-Z0-9]+)-unsplash\.jpg/g;
+        var photog_name = el.src.replace(re, '$2');
+        var photog_user = el.src.replace(re, '$3');
     
         var credit_frag = `
-            <div style="text-align: right; font-size: x-small;">
+            <div style="text-align: right; font-size: x-small; background-color: white;">
                 photo credit: <a href="https://unsplash.com/@${photog_user}" target="_blank" rel="noopener noreferrer" title="Photo courtesy of ${photog_name} on Unsplash"><span style="display:inline-block;padding:2px 3px;">${photog_name}</span></a>
             </div>
         `;
